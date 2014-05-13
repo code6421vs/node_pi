c355f3a826b503dffdbf3ddafabe86df55637027
code6421vs <code6421vs@gmail.com>
2014-05-13 11:40:28 +0800

    'test'

--- sserver.js
+++ sserver.js
@@ -1,6 +1,43 @@
 var SerialPort = require("serialport").SerialPort;
 var url = require('url');
 
+
+var STR_PAD_LEFT = 1;
+var STR_PAD_RIGHT = 2;
+var STR_PAD_BOTH = 3;
+
+function pad(str, len, pad, dir) {
+
+    if (typeof(len) == "undefined") { var len = 0; }
+    if (typeof(pad) == "undefined") { var pad = ' '; }
+    if (typeof(dir) == "undefined") { var dir = STR_PAD_RIGHT; }
+
+    if (len + 1 >= str.length) {
+
+        switch (dir){
+
+            case STR_PAD_LEFT:
+                str = Array(len + 1 - str.length).join(pad) + str;
+            break;
+
+            case STR_PAD_BOTH:
+                var right = Math.ceil((padlen = len - str.length) / 2);
+                var left = padlen - right;
+                str = Array(left+1).join(pad) + str + Array(right+1).join(pad);
+            break;
+
+            default:
+                str = str + Array(len + 1 - str.length).join(pad);
+            break;
+
+        } // switch
+
+    }
+
+    return str;
+
+}
+
 function sendData(hexStr) {
     var serialPort = new SerialPort("/dev/ttyACM0", {
         baudrate: 19200
@@ -9,6 +46,7 @@ function sendData(hexStr) {
     serialPort.open(function () {
         sys.puts("opend.")
         try {
+            hexStr = pad(hexStr, 130, "0");
             var buff = new Buffer(hexStr, "hex");
             serialPort.write(buff, function (err, results) {
                 sys.puts("results: " + results);
